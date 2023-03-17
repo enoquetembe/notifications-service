b7fb263e400ca455c123d96c409f84358445571e
enoquetembe <enoquetembetembe10@gmail.com>
2023-03-17 02:29:21 -0300

    creating post notification request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
 import { PrismaService } from './prisma.service';
 @Controller('notifications')
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello() {
+  getList() {
     return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const {recipientId, content, category} = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        recipientId,
+        content,
+        category
+      }
+    })
   }
 }
---
+++ src/create-notification-body.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty, IsUUID, Length } from "class-validator";
+
+export class CreateNotificationBody {
+    @IsNotEmpty()
+    @IsUUID()
+    recipientId: string;
+    
+    @IsNotEmpty()
+    @Length(5, 250)
+    content: string;
+
+    @IsNotEmpty()
+    category: string
+}
